51b6747e30e947b7ee1c0811f46902faa682c1a7
Geethan-K <mohan.enixs@gmail.com>
2023-06-16 15:37:37 +0530

    added jwt authentication

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const jwt = require('jsonwebtoken');
 const https = require("https");
 const cors = require('cors');
 const app = express();
-
+const router = express.Router();
+const SECRET_KEY = 'secret-key';
 app.options("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
@@ -17,11 +18,23 @@ app.use(cors());
 // Middleware to parse JSON
 app.use(express.json());
 
-// // configure the app to use bodyParser()
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(bodyParser.json());
+app.use((req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: 'Token not provided' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    // Attach the decoded payload to the request object
+    req.user = decoded;
+    next();
+  });
+});
 
 const DbUrl = 'mongodb+srv://Kakashi:SilentKiller@codevillagecluster.rbzkcod.mongodb.net/?retryWrites=true&w=majority';
 // Connect to MongoDB
@@ -34,6 +47,10 @@ mongoose.connect(DbUrl, {
 
 // User Schema
 const userSchema = new mongoose.Schema({
+  username:{
+    type:string,
+    required:true
+  },
   email: {
     type: String,
     required: true,
@@ -70,20 +87,38 @@ const User = mongoose.model('User', userSchema);
 const Admin = mongoose.model('Admin',adminSchema);
 
 
+//Middleware to verify jwt 
+/*
+function verifyToken(req,res,next){
+  const token = req.headers['authorization'];
+  if(!token){
+    res.status(403).json({message:'token not provided'})
+  }
+  jwt.verify(token,SECRET_KEY,(err,decoded)=>{
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+      // Attach the decoded payload to the request object
+      req.user = decoded;
+      next();
+  })
+}
+*/
 
 // Sample Default
 
 app.get('/', function(req, res) {
-  res.json({Name:'Kakashi',Clan:'hatake'});
+  res.json({Name:'Madhara',Clan:'Uchiha'});
 });
+
 
 
 // Register User
 app.post('/api/register', async (req, res) => {
   debugger
   try {
-    const { email, password, confirmPassword } = req.body;
-    console.log(email,password,confirmPassword)
+    const { username,email, password, confirmPassword } = req.body;
+  //  console.log(email,password,confirmPassword)
     // Check if password and confirm password match
     if (password !== confirmPassword) {
       return res.status(400).json({ message: 'Passwords do not match' });
@@ -104,6 +139,7 @@ app.post('/api/register', async (req, res) => {
 
     // Create new user
     const newUser = new User({
+      username,
       email,
       password: hashedPassword,
     });
@@ -112,7 +148,7 @@ app.post('/api/register', async (req, res) => {
     await newUser.save();
 
     // Generate JWT token
-    const token = jwt.sign({ userId: newUser._id }, 'secret-key');
+    const token = jwt.sign({ userId: newUser._id }, SECRET_KEY);
 
     // Return success response with token and points
     res.status(201).json({ token, responseType:"success",message:'Registered successfully !' });
@@ -123,7 +159,7 @@ app.post('/api/register', async (req, res) => {
 });
 
 // Login User
-app.post('/api/login', async (req, res) => {
+app.post('/api/login',async (req, res) => {
   debugger
   try {
     const { email, password } = req.body;
@@ -141,10 +177,11 @@ app.post('/api/login', async (req, res) => {
     }
 
     // Generate JWT token
-    const token = jwt.sign({ userId: user._id }, 'secret-key');
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY);
 
+    
     // Return success response with token and points
-    res.status(200).json({ token, responseType: 'success',message:'Login Success !' });
+    res.status(200).json({ Token:token, responseType: 'success',message:'Login Success !', userdata:user });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
@@ -180,7 +217,7 @@ app.post('/api/admin/registration',async(req,res)=>{
     await newAdmin.save();
 
     // Generate JWT token
-    const token = jwt.sign({ userId: newAdmin._id }, 'secret-key');
+    const token = jwt.sign({ userId: newAdmin._id }, SECRET_KEY);
 
     // Return success response with token and points
     res.status(201).json({ token,responseType:'success',message:'Admin created successfully' });
@@ -203,7 +240,7 @@ app.post('/api/admin/login',async(req,res)=>{
       }
   
       // Generate JWT token
-      const token = jwt.sign({ userId: user._id }, 'secret-key');
+      const token = jwt.sign({ userId: user._id }, SECRET_KEY);
   
       // Return success response with token and points
       res.status(200).json({ token, responseType:'success',message:'Login Successfull' });
